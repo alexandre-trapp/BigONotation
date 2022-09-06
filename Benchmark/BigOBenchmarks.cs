0b7369accb59230880d686157223a8afedca1fd9
Alexandre Cordeiro Trapp <alexandre.trapp@warren.com.br>
2022-09-06 17:48:44 -0300

    add benchmarks calls

--- Benchmark/BigOBenchmarks.cs
+++ Benchmark/BigOBenchmarks.cs
@@ -1,6 +1,7 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Mathematics;
 using BenchmarkDotNet.Order;
+using BigONotation.BigO;
 
 namespace BigONotation.Benchmark;
 
@@ -18,4 +19,40 @@ public static class BigOBenchmarks
     {
         2, 23, -575, 1, -400, 8, 44, -90, 0, 4, 180, -32, 323, 73, 59, 663, 35, 45, -67, 28, 3, 5, -5, 52, 7, 62, -20, 9, 92, 63, -342, 12, 53, 200, 234, 756, 456
     };
+
+    [Benchmark(Description = "O(1)")]
+    public static void BigO_1()
+    {
+        var isEven = BigO_1_IsEven.IsEven(9);
+    }
+
+    [Benchmark(Description = "O(log n)")]
+    public static void BigO_LogN()
+    {
+        BigO_Log_N_BinarySearch.BinarySearch(OrderedNumbers, 52);
+    }
+
+    [Benchmark(Description = "O(n)")]
+    public static void BigO_N()
+    {
+        BigO_N_SimpleLoop.SimpleLoop(84);
+    }
+
+    [Benchmark(Description = "O(n log n)")]
+    public static void BigO_NLogN()
+    {
+        BigO_NLogN_MergeSort.OrderingMergeSort();
+    }
+
+    [Benchmark(Description = "O(n^2)")]
+    public static void BigO_N2()
+    {
+        BigO_N2_NestedLoop.NestedLoop();
+    }
+
+    [Benchmark(Description = "O(2^n)")]
+    public static void BigO_2N()
+    {
+        BigO_2N_HanoiTower.TowerOfHanoi(13, 'A', 'B', 'C');
+    }
 }
