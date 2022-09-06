e24eb43cd66cd95da005e319654c34f1b1dbb214
Alexandre Cordeiro Trapp <alexandre.trapp@warren.com.br>
2022-09-06 14:07:37 -0300

    add N log N, On algorithms

---
+++ BigO/BigO_NLogN_MergeSort.cs
@@ -0,0 +1,68 @@
+﻿using BigONotation.Benchmark;
+using System.Text;
+
+namespace BigONotation.BigO;
+
+public static class BigO_NLogN_MergeSort
+{
+    // O(n log n)
+    public static void OrderingMergeSort()
+    {
+        var numbersMergeSort = MergeSort(BigOBenchmarks.UnorderedNumbers.ToArray<int>(), 0, BigOBenchmarks.UnorderedNumbers.Count -1).ToList<int>();
+
+        var sbMergeSort = new StringBuilder();
+
+        for (int count = 0; count < numbersMergeSort.Count; count++)
+        {
+            sbMergeSort.Append(numbersMergeSort[count].ToString() + "==>");
+        }
+
+        Console.WriteLine(sbMergeSort.ToString());  
+    }
+
+    private static int[] MergeSort(int[] inputItems, int leftBound, int rightBound)
+    {
+        if (leftBound > rightBound)
+            return inputItems;
+
+        int middle = (leftBound + rightBound) /2;
+
+        MergeSort(inputItems, leftBound, middle);
+        MergeSort(inputItems, middle + 1, rightBound);
+
+        int[] leftArray = new int[middle - leftBound +1];
+        int[] rightArray = new int[rightBound - middle];
+
+        Array.Copy(inputItems, leftBound, leftArray, 0, middle - leftBound +1);
+        Array.Copy(inputItems, middle + 1, rightArray, 0, rightBound - middle);
+
+        int i = 0;
+        int j = 0;
+
+        for (int count = leftBound; count < rightBound +1; count++)
+        {
+            if (i == leftArray.Length)
+            {
+                inputItems[count] = rightArray[j];
+                j++;
+            }
+            else if (j == rightArray.Length)
+            {
+                inputItems[count] = leftArray[i];
+                i++;
+            }
+            else if (leftArray[i] <= rightArray[j])
+            {
+                inputItems[count] = leftArray[i];
+                i++;
+            }
+            else
+            {
+                inputItems[count] = rightArray[j];
+                j++;
+            }
+        }
+
+        return inputItems;
+    }
+}
---
+++ BigO/BigO_N_SimpleLoop.cs
@@ -0,0 +1,18 @@
+﻿using BigONotation.Benchmark;
+
+namespace BigONotation.BigO;
+
+public static class BigO_N_SimpleLoop
+{
+    public static bool SimpleLoop(int searchedNumber)
+    {
+        // O(n)
+        foreach (var element in BigOBenchmarks.OrderedNumbers)
+        {
+            if (element == searchedNumber)
+                return true;
+        }
+
+        return false;
+    }
+}
