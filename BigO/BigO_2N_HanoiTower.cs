0d29c5f0381cde281790755bc90682d2e721e0bb
Alexandre Cordeiro Trapp <alexandre.trapp@warren.com.br>
2022-09-06 17:38:49 -0300

    add new algorithms

---
+++ BigO/BigO_2N_HanoiTower.cs
@@ -0,0 +1,17 @@
+﻿namespace BigONotation.BigO;
+
+public static class BigO_2N_HanoiTower
+{
+    public static void TowerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+    {
+        if (n == 0)
+            return;
+
+        // O(2^n)
+        TowerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+
+        Console.WriteLine($"Move disk {n} from rod {from_rod} to rod {to_rod}");
+
+        TowerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+    }
+}
---
+++ BigO/BigO_N2_NestedLoop.cs
@@ -0,0 +1,25 @@
+﻿using BigONotation.Benchmark;
+
+namespace BigONotation.BigO;
+
+public static class BigO_N2_NestedLoop
+{
+    public static void NestedLoop()
+    {
+        var n2Array = new int[20000];
+
+        int counter = 0;
+
+        // O(n * n) = O(n²)
+
+        for (int i = 0; i < n2Array.Length; i++)         // O(n)
+        {
+            for (int j = 0; j < n2Array.Length; j++)     // O(n)
+            {
+                counter++;
+            }
+        }
+
+        Console.WriteLine("O(n^2) finished in " + counter.ToString() + " cycles.");
+    }
+}
