2b1d42407c9aaf8603cb229fb90f65e5d2680e86
Alexandre Cordeiro Trapp <alexandre.trapp@warren.com.br>
2022-09-09 17:42:26 -0300

    revert to BenchmarkRunner.Run<BigOBenchmarks>();

--- Program.cs
+++ Program.cs
@@ -2,5 +2,4 @@
 using BigONotation.Benchmark;
 using BigONotation.BigO;
 
-new BigO_NLogN_MergeSort().OrderingMergeSort();
-//BenchmarkRunner.Run<BigOBenchmarks>();
+BenchmarkRunner.Run<BigOBenchmarks>();
